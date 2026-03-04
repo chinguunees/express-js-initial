9b624423b81a9e3a2094fdb32a11b97775b2881a
Chinguunee <u.chingunn@gmail.com>
2026-03-04 11:34:55 +0800

    second

--- server.ts
+++ server.ts
@@ -4,77 +4,62 @@ import express from "express";
 const server = express();
 const port = 3001;
 
-const books = [
-  {
-    id: 1,
-    title: "Harry Potter",
-    author: "JJK",
-  },
-  {
-    id: 2,
-    title: "Harry Potter 2",
-    author: "JJK",
-  },
-];
 server.use(express.json());
+
 server.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Ok" });
 });
 
-server.get("/books", (req: Request, res: Response) => {
-  res.status(200).json(books);
-});
-server.get("/books/:id", (req: Request, res: Response) => {
+server.get("/student/:name", (req: Request, res: Response) => {
   const params = req.params;
-  const id = params.id;
-  const book = books.find((book) => String(book.id) === String(id));
-  res.status(200).json(book);
+  res.status(200).send({ message: `Сайн байна уу,[${params.name}]` });
 });
 
-server.post("/books", (req: Request, res: Response) => {
-  const { title, author } = req.body;
-
-  const newBookId = books.length + 1;
-
-  const newBook = { id: newBookId, title, author };
-
-  books.push(newBook);
-
-  res.status(200);
-
-  res.send(books);
-  console.log(books);
+server.get("/filter", (req: Request, res: Response) => {
+  const query = req.query;
+  res.status(200).json({ message: "amjilttai", query });
 });
 
-server.put("/books/:id", (req: Request, res: Response) => {
-  const params = req.params;
-  const id = params.id;
-  const { title, author } = req.body;
-  // const updatedBooks = books.map((book) =>
-  //   String(book.id) === String(id) ? { ...book, title, author } : book,
-  // );
-  const updatedBooks = books.map((book) => {
-    if (String(book.id) === id) {
-      const updatedBook = {
-        id: book.id,
-        title: title,
-        author: author,
-      };
-      return updatedBook;
-    } else {
-      return book;
-    }
-  });
-
-  res.status(200).send(updatedBooks);
-});
-
-server.delete("/delete/:type", (req: Request, res: Response) => {
-  const params = req.params;
+server.get("/header", (req: Request, res: Response) => {
   const headers = req.headers;
-  console.log(params, headers);
-  res.status(200).json({ message: "deletetest" });
-  res.send("Delete methodoor testlej baina");
+  console.log(headers.authorization);
+
+  res.status(200).send("ok");
+});
+
+server.all("/duruv", (req: Request, res: Response) => {
+  const path = req.path;
+  const method = req.method;
+  res.status(200).send(`Ta ${path} zamruu ${method} huselt ilgeelee.`);
+});
+
+server.get("/library/:category/:id", (req: Request, res: Response) => {
+  const headers = req.headers;
+  const { id, category } = req.params;
+  // const id = params.id;
+  const method = req.method;
+  const path = req.path;
+  // const category = params[1];
+  const query = req.query;
+
+  const authorization = headers.authorization?.split(" ")[1];
+  if (authorization === "test") {
+    headers["x-api-key"] = authorization;
+  } else {
+    res.send("Not verified");
+  }
+
+  res.status(200).send({
+    status: "Success",
+    requestInfo: {
+      method,
+      path,
+      category,
+      id,
+      query,
+      auth: "Verified",
+    },
+  });
 });
 
 server.listen(port, () => {
