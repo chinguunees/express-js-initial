7a5fa786031a7560896a9b7e192e05a48ce1513b
Chinguunee <u.chingunn@gmail.com>
2026-03-12 11:51:31 +0800

    added DB

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import fs from "node:fs/promises";
 import express from "express";
 import { getBooks } from "./utils/get-books";
-
+import { prisma } from "./lib/prisma";
 type Book = {
   id: number;
   title: string;
@@ -13,45 +13,46 @@ const port = 3001;
 
 server.use(express.json());
 
-server.get("/books", async (req: Request, res: Response) => {
-  const books = await getBooks();
+server.get("/users", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
 
-  res.status(200).json({ message: "Ok", books });
+  res.status(200).json({ message: "Ok", user });
 });
-server.get("/books/:id", async (req: Request, res: Response) => {
+server.get("/users/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-  const books = await getBooks();
-  const book = books.find((book) => String(book.id) === id);
+  const user = await prisma.user.findFirst({
+    where: { id: Number(id) },
+  });
 
-  res.status(200).json({ message: "Ok", book });
+  res.status(200).json({ message: "Ok", user });
 });
 
-server.post("/books/", async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const books: BookType[] = await getBooks();
-  const newBookId = books.length + 1;
-  const newBook = { id: newBookId, title };
-  books.push(newBook);
+server.post("/users", async (req: Request, res: Response) => {
+  const { email, password, age, role } = req.body;
+  const user = await prisma.user.create({
+    data: { email, password, age, role },
+  });
 
-  const writeJsonFile = async () => {
-    try {
-      await fs.writeFile("./db.json", JSON.stringify(books), "utf8");
-      console.log("File created successfully");
-    } catch (err) {
-      console.error("Error writing file:", err);
-    }
-  };
+  res.status(200).json({ message: "Амжилттай", user });
+});
+server.delete("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.delete({
+    where: { id: Number(id) },
+  });
+  res.status(200).json({ message: "OK", user });
+});
 
-  writeJsonFile();
-
-  res.status(200).json({ message: "Амжилттай шинэ ном нэмэгдлээ", books });
+server.put("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { email, password, age } = req.body;
+  const user = await prisma.user.update({
+    where: { id: Number(id) },
+    data: { email, password, age },
+  });
+  res.status(200).json({ message: "OK", user });
 });
 
 server.listen(port, () => {
   console.log(`"Server aslaa", http://localhost:${port}`);
 });
-
-type BookType = {
-  id: number;
-  title: string;
-};
