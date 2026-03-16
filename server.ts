addb227e09b184d7028d27e67b921b4dbdf7a91f
Chinguunee <u.chingunn@gmail.com>
2026-03-16 11:42:03 +0800

    more cruds

--- server.ts
+++ server.ts
@@ -53,6 +53,84 @@ server.put("/users/:id", async (req: Request, res: Response) => {
   res.status(200).json({ message: "OK", user });
 });
 
+/////////// FOOOOOD CATEGORY
+
+server.get("/category", async (req: Request, res: Response) => {
+  const category = await prisma.foodCategory.findMany({
+    include: {
+      foods: true,
+    },
+  });
+  res.status(200).json({ message: "Ok", category });
+});
+
+server.post("/category", async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const category = await prisma.foodCategory.create({
+    data: { name },
+  });
+
+  res.status(200).json({ message: "Амжилттай", category });
+});
+
+server.delete("/category/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const category = await prisma.foodCategory.delete({
+    where: { id: Number(id) },
+  });
+  res.status(200).json({ message: "OK", category });
+});
+
+server.put("/category/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const category = await prisma.foodCategory.update({
+    where: { id: Number(id) },
+    data: { name },
+  });
+  res.status(200).json({ message: "OK", category });
+});
+
+//////// FOOOOOOOODS
+
+server.get("/foods/", async (req: Request, res: Response) => {
+  const foods = await prisma.food.findMany();
+  res.status(200).json({ message: "Ok", foods });
+});
+
+server.post("/foods", async (req: Request, res: Response) => {
+  const { name, price, foodCategoryId } = req.body;
+  const foods = await prisma.food.create({
+    data: { name, price, foodCategoryId },
+  });
+
+  res.status(200).json({ message: "Амжилттай", foods });
+});
+
+server.delete("/foods/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const foods = await prisma.food.delete({
+    where: { id: Number(id) },
+  });
+  res.status(200).json({ message: "OK", foods });
+});
+
+server.put("/foods/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, price, foodCategoryId } = req.body;
+  try {
+    const food = await prisma.food.update({
+      where: { id: Number(id) },
+      data: { name, price, foodCategoryId },
+    });
+
+    res.status(200).json({ message: "OK", food });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+});
+
 server.listen(port, () => {
   console.log(`"Server aslaa", http://localhost:${port}`);
 });
