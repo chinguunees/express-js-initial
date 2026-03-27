1b7eafab63378dedb2dcc44b293c008d9e01cad1
Chinguunee <u.chingunn@gmail.com>
2026-03-27 11:03:27 +0800

    build

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,8 +1,3 @@
-import { getCategory } from "@/lib/api";
-import { FoodCard } from "./components/FoodCard";
-import { Button } from "@/components/ui/button";
-import Addfood from "./components/AddFood";
-
 const Home = async () => {
   return <div></div>;
 };
